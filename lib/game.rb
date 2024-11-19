a737448a7c3e956799062f4791bf558e1f2428e7
shaquinbale <scopelenskingdra@gmail.com>
2024-11-19 15:27:04 -0500

    Finish main game logic

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,11 @@
-require 'set'
+require 'json'
 require_relative 'hangman'
 
 class Game
   def initialize(word_list)
       @game_state = {
       word: word_list.sample,
-      letters_guessed: Set.new,
+      letters_guessed: Array.new,
       guesses: 0
     }
   end
@@ -18,14 +18,14 @@ class Game
       word_hidden[index] = word[index] if @game_state[:letters_guessed].include?(word[index])
     end
 
-    puts word_hidden
+    puts word_hidden.join
   end
 
   def get_guess
     puts "Guess another letter!"
     input = gets.downcase.chomp
 
-    until input.length == 1 && input =~ [/a-z/]
+    until input.length == 1 && input =~ /[a-z]/
       puts "Your guess must be one letter"
       input = gets.downcase.chomp
     end
@@ -39,21 +39,30 @@ class Game
   end
 
   def update_game(guess)
-    @game_state[letters_guessed].add guess
+    @game_state[:letters_guessed] << guess
+    @game_state[:guesses] += 1
   end
 
-  def game_won?
-    
+  def check_for_win
+    @game_state[:word].chars.all? { |char| @game_state[:letters_guessed].include?(char)}
   end
 
-  def end_game
-    
+  def game_won
+    puts "You win! The word was #{@game_state[:word]}"
+  end
+
+  def game_lost
+    puts "You lose. You'll never figure out what the word was"
   end
 
   def play_round
+    puts "Letters guessed: #{@game_state[:letters_guessed].sort.join}"
     puts Hangman::HANGMANPICS[@game_state[:guesses]]
+    return game_lost if @game_state[:guesses] >= 6
+
     display_word
     update_game(get_guess)
-    game_won? ? end_game : play_round
+
+    check_for_win ? game_won : play_round
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -3,4 +3,4 @@ require_relative 'game'
 word_list = File.readlines('word_list.txt').map(&:chomp)
 game = Game.new(word_list)
 
-puts Hangman::HANGMANPICS[0]
+game.play_round
